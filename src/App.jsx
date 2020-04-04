475f78814edf95b86ecbcdd1b907ced92675f73f
Tasnuva <thaque_ubc@hotmail.com>
2020-04-03 18:48:51 -0700

    navbar and footer is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './App.css'
-import { HashRouter, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AV from 'leancloud-storage'
 import Home from './pages/Home'
 import LogIn from './pages/LogIn'
-import Todo from './pages/Todo'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from './pages/Footer'
 import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default class App extends React.Component {
   constructor (props) {
@@ -16,25 +16,16 @@ export default class App extends React.Component {
 
   render () {
     return (
-      <Navbar/>
-      // <HashRouter hashType='hashbang'>
-      //   <nav>
-      //     <Link to='/'>Home</Link>
-      //     <Link to='/login'>Log in</Link>
-      //     <Link to='/todo'>Todo</Link>
-      //   <Switch>
-      //     <Route path='/' exact>
-      //       <Home />
-      //     </Route>
-      //     <Route path='/login' exact>
-      //       <LogIn />
-      //     </Route>
-
-      //     <Route path='/todo' exact>
-      //       <Todo />
-      //     </Route>
-      //   </Switch>
-      // </HashRouter>
-    )
+      <Router>
+      <div>
+      <Navbar />
+      <Switch>
+         <Route exact path="/" component={Home} />
+         <Route path="/logIn" component={LogIn} />
+       </Switch>
+       {/* <Footer/> */}
+      </div>
+  </Router>
+);
   }
 }
