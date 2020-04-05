e96f4bf361c18bd8e2f35eb682b2f2f152be28c9
TonyKoo <TonyKoo@msn.com>
2020-04-05 00:33:01 -0700

    Category Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import AV from 'leancloud-storage'
-import Home from './pages/Home'
-import LogIn from './pages/LogIn'
-import Footer from './pages/Footer'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Verify from './pages/Verify'
-import Navbar from './components/Navbar'
-
+import AV from "leancloud-storage";
+import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
+import Footer from "./pages/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Verify from "./pages/Verify";
+import Navbar from "./components/Navbar";
+import Categories from "./pages/Categories";
 
 export default class App extends React.Component {
-  constructor (props) {
-    super(props)
-    AV.init(require('./av-config.json'))
+  constructor(props) {
+    super(props);
+    AV.init(require("./av-config.json"));
   }
 
-  render () {
+  render() {
     return (
       <Router>
-      <div>
-      <Navbar />
-      <Switch>
-         <Route exact path="/" component={Home} />
-         <Route path="/logIn" component={LogIn} />
-         <Route path="/verify" component={Verify} />
-       </Switch>
-       {/* <Footer/> */}
-      </div>
-  </Router>
-);
+        <div>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/logIn" component={LogIn} />
+            <Route path="/verify" component={Verify} />
+            <Route path="/categories" component={Categories} />
+          </Switch>
+          {/* <Footer/> */}
+        </div>
+      </Router>
+    );
   }
 }
---
+++ src/pages/Categories.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+import AV from "leancloud-storage";
+
+export default class Categories extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userCategories: [],
+      newCategoryEntry: "",
+      isLoading: true,
+    };
+    this.user = AV.User.current();
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  fetchCategories = async () => {
+    this.setState({ isLoading: true });
+    var UserCategories = AV.Object.extend("Categories");
+
+    try {
+      var query = new AV.Query("Categories");
+      query.equalTo("username", this.user.getUsername());
+
+      await query.find().then((queryResult) => {
+        console.log(queryResult[0].attributes.userCategories);
+        this.setState({
+          userCategories: queryResult[0].attributes.userCategories,
+        });
+      });
+    } catch (error) {
+      console.log(JSON.stringify(error));
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  addCategory = async () => {
+    let firstInsert = true;
+    var UserCategories = AV.Object.extend("Categories");
+    var userCategories = new UserCategories();
+
+    let categoryToAdd = this.state.newCategoryEntry.trim();
+    if (categoryToAdd.length === 0) {
+      alert("Please Enter A Category Before Adding");
+      return;
+    } else {
+      categoryToAdd =
+        categoryToAdd[0].toUpperCase() + categoryToAdd.slice(1).toLowerCase();
+    }
+
+    try {
+      var query = new AV.Query("Categories");
+      query.equalTo("username", this.user.getUsername());
+
+      await query.find().then((queryResult) => {
+        userCategories = queryResult[0];
+        console.log(userCategories.attributes);
+      });
+
+      firstInsert = false;
+    } catch (error) {
+      console.log(JSON.stringify(error));
+    }
+
+    if (firstInsert) {
+      userCategories.set("username", this.user.getUsername());
+    }
+
+    userCategories.addUnique("userCategories", categoryToAdd);
+
+    console.log(userCategories);
+    userCategories.save().then((response) => {
+      this.fetchCategories();
+      this.setState({
+        newCategoryEntry: "",
+      });
+    });
+  };
+
+  removeCategory = async (categoryToRemove) => {
+    var UserCategories = AV.Object.extend("Categories");
+    var userCategories = new UserCategories();
+
+    try {
+      var query = new AV.Query("Categories");
+      query.equalTo("username", this.user.getUsername());
+
+      await query.find().then((queryResult) => {
+        userCategories = queryResult[0];
+        console.log(userCategories.attributes);
+      });
+
+      userCategories.remove("userCategories", categoryToRemove);
+      console.log(userCategories);
+    } catch (error) {
+      console.log(JSON.stringify(error));
+    }
+
+    userCategories.save().then((response) => this.fetchCategories());
+  };
+
+  handleCategoryEntry = (e) => {
+    this.setState({
+      newCategoryEntry: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h3>Manage Categories for {this.user.getUsername()}</h3>
+        <br />
+        <input
+          value={this.state.newCategoryEntry}
+          onChange={this.handleCategoryEntry}
+        />
+        <button onClick={this.addCategory}>Add</button>
+
+        {this.state.isLoading && <h5>Loading...</h5>}
+
+        {this.state.userCategories.length > 0 && !this.state.isLoading && (
+          <React.Fragment>
+            <h4>My Categories</h4>
+            <ul>
+              {this.state.userCategories.map((category, index) => (
+                <li key={index}>
+                  {category}{" "}
+                  <button onClick={() => this.removeCategory(category)}>
+                    X
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </React.Fragment>
+        )}
+
+        {this.state.userCategories.length == 0 && !this.state.isLoading && (
+          <h4>No Categories</h4>
+        )}
+      </div>
+    );
+  }
+}
