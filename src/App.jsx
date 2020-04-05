6c07ee00af47b72e9526d00ab8c1e46fc5e17655
lm902 <i@lm902.cn>
2020-04-05 15:55:56 -0700

    standard --fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import AV from 'leancloud-storage'
 import Home from './pages/Home'
 import LogIn from './pages/LogIn'
-import Footer from './pages/Footer'
 import Navbar from './components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Verify from './pages/Verify'
-
-
 
 export default class App extends React.Component {
   constructor (props) {
@@ -20,16 +17,16 @@ export default class App extends React.Component {
   render () {
     return (
       <Router>
-      <div>
-      <Navbar />
-      <Switch>
-         <Route exact path="/" component={Home} />
-         <Route path="/logIn" component={LogIn} />
-         <Route path="/verify" component={Verify} />
-       </Switch>
-       {/* <Footer/> */}
-      </div>
-  </Router>
-);
+        <div>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/logIn' component={LogIn} />
+            <Route path='/verify' component={Verify} />
+          </Switch>
+          {/* <Footer/> */}
+        </div>
+      </Router>
+    )
   }
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import AV from 'leancloud-storage'
-import Navbar from '../components/Navbar'
-
 
 export default class Home extends React.PureComponent {
   render () {
     const user = AV.User.current()
     return (
-      <div className="container">
+      <div className='container'>
         <h1> Welcome !!! </h1>
         {user && `Signed in as ${user.getUsername()}`}
         {user && <button onClick={() => AV.User.logOut().then(() => this.forceUpdate())}>Sign out</button>}
